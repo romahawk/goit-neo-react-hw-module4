b25ceb5a9eb30fb6c90559d1f95e6cc7a00c324e
Roman Mazuryk <romazuryk@gmail.com>
2025-09-21 18:58:09 +0200

    fix(ImageCard): move click handler to <img> per spec

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,11 +1,18 @@
 import css from "./ImageCard.module.css";
 
 export default function ImageCard({ image, onClick }) {
-  const src = image.urls?.small;
-  const alt = image.alt_description ?? "Image";
+  const src = image?.urls?.small ?? image?.urls?.thumb ?? "";
+  const alt = image?.alt_description ?? "Image";
+
   return (
-    <div className={css.card} onClick={onClick} role="button" tabIndex={0}>
-      <img className={css.img} src={src} alt={alt} loading="lazy" />
+    <div className={css.card}>
+      <img
+        className={css.img}
+        src={src}
+        alt={alt}
+        loading="lazy"
+        onClick={onClick}
+      />
     </div>
   );
 }
